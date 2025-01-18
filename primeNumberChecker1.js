97a2a01ab35d53f92c5b30a38391674ef3b5c4ec
Vasil-Antonov1234 <vaskoantonov1234@gmail.com>
2025-01-18 20:17:12 +0200

    Create primeNumberChecker1.js

---
+++ primeNumberChecker1.js
@@ -0,0 +1,14 @@
+function solve(num) {
+    let isPrime = true;
+    
+    for (let divisor = 2; divisor < num; divisor++) {
+        if (num % divisor === 0) {
+            isPrime = false;
+        }
+    }
+
+    console.log(isPrime);
+}
+
+
+solve(147);
